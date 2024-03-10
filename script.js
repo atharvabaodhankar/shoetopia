ace1a93211fb5a0a194279a14bc92aaec5b1ee2f
AtharvaPC <baodhankaratharva@gmail.com>
2024-03-10 09:39:47 +0530

    added hamburger

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
 var navbar = document.querySelector("nav");
+var swiperHero = document.querySelector(".swiper-hero");
+var hamburger = document.querySelector(".toggle-bar");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navbar.classList.toggle("active");
+  })
 
 window.addEventListener('scroll', function () {
   var sampleSection = document.querySelector('#sample');
   var rect = sampleSection.getBoundingClientRect();
     if (rect.top <= window.innerHeight / 2 && rect.bottom >= 0) {
-        navbar.classList.add('sticky-bar');
+      navbar.classList.add('sticky-bar');
+      swiperHero.style.marginTop = '8rem';
     }
     else
   {
     navbar.classList.remove('sticky-bar');
+    swiperHero.style.marginTop = '0';
       
       }
 });
 
+
+
 var swiper = new Swiper(".hero-swiper", {
     autoplay: {
-        enabled: true,
+        enabled: false,
         delay: 5000,          
         pauseOnMouseEnter: true,
         disableOnInteraction: false,
